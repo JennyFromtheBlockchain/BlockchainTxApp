7ae19c312ff2b5f4edd4b0b064618e3fc7e7b08c
user <user@localhost.localdomain>
2018-11-24 18:56:03 +0000

    Adding zcash

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var blockchainCaller = require('./blockchair_api_caller.js');
 var trxCaller = require('./trx_api_caller.js');
 var chainSoCaller = require('./chain_so_api_caller.js');
 var dogechainCaller = require('./dogechain_api_caller.js');
+var zchainCaller = require('./zchain_api_caller.js');
 
 var interval = setInterval(function(){
-	//blockchainCaller.getData();
-	//trxCaller.getData();
 	//chainSoCaller.getData();
+	blockchainCaller.getData();
+	trxCaller.getData();
 	dogechainCaller.getData();
+	zchainCaller.getData();
 }, 5000);
---
+++ zchain_api_caller.js
@@ -0,0 +1,56 @@
+const service = require('./service.js');
+const network_obj = require('./network_object');
+const mysql = require("mysql");
+const axios = require("axios");
+const blocksPerCall = 20;
+const pool = mysql.createPool({
+  connectionLimit: 5,
+  host: "localhost",
+  user: "root",
+  //password: "password",
+  database: "transactions"
+});
+
+function processBlock(blockData) {
+    var date = new Date(blockData.timestamp);
+    var zecNo = new network_obj(
+      "zec",
+      blockData.height,
+      blockData.transactions,
+      blockData.timestamp
+    );
+    service.persist(zecNo);
+  }
+
+function getData() {
+  pool.getConnection(function(err, connection) {
+    if (err) {
+      return console.log("error: " + err.message);
+    }
+    var query = "select max(blockNumber) from zec_network;";
+    connection.query(query, function(err, result, fields) {
+      if (err) throw err;
+      var maxBlockInDb = parseInt(service.getBlockNumberFromRowDataPacket(result));
+      callApiForBlocks(maxBlockInDb);
+    });
+    connection.release();
+  });
+}
+function callApiForBlocks(maxBlockInDb) {
+  axios
+    .get("https://api.zcha.in/v2/mainnet/blocks?sort=height&direction=descending&limit=" + blocksPerCall + "&offset=0")
+    .then(response => {
+      for (var i = 0; i < response.data.length; i++) {
+        processBlock(response.data[i]);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+module.exports = {
+    getData: function() {
+      //msleep(500);
+      getData();
+    }
+};
