d9bd594663b9ac8a353b4e7dff013ea37ba8f3a0
user <user@localhost.localdomain>
2018-12-01 13:57:37 +0000

    Adding angular material

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +30,14 @@ import { BlockchainComponent } from './blockchain/blockchain.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    //HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
