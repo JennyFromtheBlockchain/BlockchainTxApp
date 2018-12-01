5d92dcebbf32957e037013b0d8707a0bc1f5a095
user <user@localhost.localdomain>
2018-12-01 15:11:28 +0000

    Adding blcokchain data to blockchain page

--- ui/src/app/blockchain/blockchain.component.ts
+++ ui/src/app/blockchain/blockchain.component.ts
@@ -30,18 +30,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+var ELEMENT_DATA: PeriodicElement[];
 
 /**
  * @title Basic CDK data-table
@@ -56,9 +45,28 @@ const ELEMENT_DATA: PeriodicElement[] = [
 //   styleUrls: ['cdk-table-basic-example.css'],
 //   templateUrl: 'cdk-table-basic-example.html',
 // })
-export class BlockchainComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new ExampleDataSource();
+export class BlockchainComponent implements OnInit {
+
+  dataSource: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getBlockchain().subscribe(data => {
+        //this.dataSource = data
+        //console.log(this.dataSource);
+        ELEMENT_DATA = [data[1], data[2]];
+        this.dataSource = new ExampleDataSource();
+        console.log(this.dataSource);
+        console.log(ELEMENT_DATA);
+        console.log(data);
+      }
+    );
+  }
+
+  //displayedColumns: string[] = ['position'];
+  displayedColumns: string[] = ['blockchainTicker', 'blockNumber', 'transactions', 'timestamp'];
+  // dataSource = new ExampleDataSource();
 }
 
 /**
